6c022ee82b32a831b1982371fa8601242bd06c9b
Spartanc08 <49616658+Nishanthc08@users.noreply.github.com>
2023-05-16 11:22:23 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var header = document.createElement("H1");
+    var text = document.createTextNode("Hello Nishanth!");
+
+    header.appendChild(text);
+    document.body.appendChild(header);
+}
